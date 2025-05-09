19fb674a8781e1920129206e4bc579c125b06063
Robani_Diansyah <melinting522@gmail.com>
2025-05-09 14:44:02 +0700

    menambahkan nama Robani Diansyah

--- app/src/main/java/com/app/literaku/MainActivity.kt
+++ app/src/main/java/com/app/literaku/MainActivity.kt
@@ -19,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         print("saya mahasiswa")
         print("23312191")
 
+        print ("Nama : Robani Diansyah")
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
