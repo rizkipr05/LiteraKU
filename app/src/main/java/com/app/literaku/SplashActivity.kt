42d7c262b07c679560a4f664e7820c20fe1f8e30
rizkipr05 <rizki.qq05@gmail.com>
2025-05-16 15:17:05 +0700

    merubah nama file

--- app/src/main/java/com/app/literaku/SplashActivity.kt
+++ app/src/main/java/com/app/literaku/SplashActivity.kt
@@ -6,23 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-
-
-        print("hello word")
-        print("rizki pratama")
-        print("saya mahasiswa")
-        print("23312191")
-
-        print ("Nama : Robani Diansyah")
-        print("woi p")
-
-
+        setContentView(R.layout.activity_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
