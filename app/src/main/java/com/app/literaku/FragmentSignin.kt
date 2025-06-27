fffd6fe1c2c660de00249f9ca94ba062e288f2f7
rizkipr05 <rizki.qq05@gmail.com>
2025-06-27 23:50:41 +0700

    benerin kode FragmentSignin.kt eror bagian  // Optionally, show email and password in Toast for testing                                                        Toast.makeText(requireContext(), "Email: $userEmail, Password: $userPassword", Toast.LENGTH_SHORT).show() ini ternyata belum diimport

--- app/src/main/java/com/app/literaku/FragmentSignin.kt
+++ app/src/main/java/com/app/literaku/FragmentSignin.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.app.literaku.databinding.FragmentSigninBinding
+import android.widget.Toast
 
 class FragmentSignin : Fragment() {
 
