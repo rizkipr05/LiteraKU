398f8a569480556afa050f383399280c5fc1d495
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2025-06-28 14:29:45 +0700

    menghapus binding sebelumnya

--- app/src/main/java/com/app/literaku/HomeActivity.kt
+++ app/src/main/java/com/app/literaku/HomeActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.app.literaku.databinding.ActivityHomeBinding
-import com.app.literaku.KategoriFragment
+
 
 class HomeActivity : AppCompatActivity() {
 
@@ -15,15 +15,7 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val kategoriButton = findViewById<LinearLayout>(R.id.layout_categories)
 
-        kategoriButton.setOnClickListener {
-            // Contoh: Pindah ke fragment
-            val fragment = KategoriFragment()
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, fragment)
-                .addToBackStack(null)
-                .commit()
-        }
     }
 }
+
